11204da39b4a0547ab69636c52b7b3a5ce55eca7
joyluiz63 <joyluiz63@gmail.com>
2020-04-29 13:11:19 -0300

    Criado o projeto, configurado local e timezone, criado models

---
+++ database/migrations/2020_04_29_125345_create_prorrogas_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProrrogasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('prorrogas', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('prorrogas');
+    }
+}
